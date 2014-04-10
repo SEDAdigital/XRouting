b2765d6489134658af192ac790c43d0865751615
Joakim Nyman <joakim@fractalwolfe.com>
2014-04-10 17:31:21 +0300

    Added system settings to build script, with english and swedish lexicons for descriptions.

---
+++ core/components/xrouting/lexicon/en/default.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Default English lexicon
+ */
+
+// System settings
+$_lang['setting_xrouting.include_www'] = 'Include WWW-subdomain';
+$_lang['setting_xrouting.include_www_desc'] = 'Specifies if the www-subdomain should automatically be included when matching against the base domain, ie. www.example.com should return the same context as example.com.';
+$_lang['setting_xrouting.default_context'] = 'Default context';
+$_lang['setting_xrouting.default_context_desc'] = 'The default context to redirect to if no matches were found and xrouting.show_no_match_error is set to No.';
+$_lang['setting_xrouting.show_no_match_error'] = 'Return error messages';
+$_lang['setting_xrouting.show_no_match_error_desc'] = 'If set to yes, XRouting will return an error instead of redirecting to the default context.';
