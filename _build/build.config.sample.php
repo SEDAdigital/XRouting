2f8d6ae88422917a35da5c7d36579a98d3912177
Christian Seel <cs@chsmedien.de>
2014-04-17 10:24:57 +0200

    add missing slash to base path

--- _build/build.config.sample.php
+++ _build/build.config.sample.php
@@ -1,7 +1,7 @@
 <?php
 // Set this path to your MODX install location
 // Default assumes source code in /modx/xrouting/
-define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))));
+define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))).'/');
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
