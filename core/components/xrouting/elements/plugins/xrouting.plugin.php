77290a1b55cc21524a7a6d039af2db377fc00327
Christian Seel <cs@chsmedien.de>
2015-05-20 10:25:22 +0200

    removed constant MODX_BASE_URL and added optional debug info output

--- core/components/xrouting/elements/plugins/xrouting.plugin.php
+++ core/components/xrouting/elements/plugins/xrouting.plugin.php
@@ -5,12 +5,12 @@ switch ($modx->event->name) {
     case 'OnContextSave':
     case 'OnContextRemove':
     case 'OnSiteRefresh':
-		
-		$contexts = array();
+        
+        $contexts = array();
         $cacheKey = 'xrouting_contextmap';
         $cacheOptions = array();
         
-    	// build context array
+        // build context array
         $query = $modx->newQuery('modContext');
         $query->where(array('modContext.key:NOT IN' => array('mgr')));
         $query->sortby($modx->escape('modContext') . '.' . $modx->escape('key'), 'DESC');
@@ -23,19 +23,19 @@ switch ($modx->event->name) {
             }
             
             if (!empty($contextSettings['http_host']) && !empty($contextSettings['base_url'])) {
-            	
-            	// add http_host to hosts list
-            	$contexts['_hosts'][$contextSettings['http_host']][] = $context->get('key');
-            	
-            	// add alias hosts to host list
-            	if (!empty($contextSettings['http_host_aliases'])) {
-                	foreach (explode(',',$contextSettings['http_host_aliases']) as $alias) {
-	                	$contexts['_hosts'][$alias][] = $context->get('key');
-                	}
-            	}
-            	
-            	// add context settings
-            	$contexts[$context->get('key')] = $contextSettings;
+                
+                // add http_host to hosts list
+                $contexts['_hosts'][$contextSettings['http_host']][] = $context->get('key');
+                
+                // add alias hosts to host list
+                if (!empty($contextSettings['http_host_aliases'])) {
+                    foreach (explode(',',$contextSettings['http_host_aliases']) as $alias) {
+                        $contexts['_hosts'][$alias][] = $context->get('key');
+                    }
+                }
+                
+                // add context settings
+                $contexts[$context->get('key')] = $contextSettings;
             }
         }
          
@@ -56,7 +56,7 @@ switch ($modx->event->name) {
         $contexts = $modx->cacheManager->get($cacheKey, $cacheOptions);
         
         if (empty($contexts)) {
-        	// build context array
+        // build context array
             $query = $modx->newQuery('modContext');
             $query->where(array('modContext.key:NOT IN' => array('mgr')));
             $query->sortby($modx->escape('modContext') . '.' . $modx->escape('key'), 'DESC');
@@ -69,80 +69,91 @@ switch ($modx->event->name) {
                 }
                 
                 if (!empty($contextSettings['http_host']) && !empty($contextSettings['base_url'])) {
-                	
-                	// add http_host to hosts list
-                	$contexts['_hosts'][$contextSettings['http_host']][] = $context->get('key');
-                	
-                	// add alias hosts to host list
-                	if (!empty($contextSettings['http_host_aliases'])) {
-	                	foreach (explode(',',$contextSettings['http_host_aliases']) as $alias) {
-		                	$contexts['_hosts'][$alias][] = $context->get('key');
-	                	}
-                	}
-                	
-                	// add context settings
-                	$contexts[$context->get('key')] = $contextSettings;
+                    
+                    // add http_host to hosts list
+                    $contexts['_hosts'][$contextSettings['http_host']][] = $context->get('key');
+                    
+                    // add alias hosts to host list
+                    if (!empty($contextSettings['http_host_aliases'])) {
+                        foreach (explode(',',$contextSettings['http_host_aliases']) as $alias) {
+                            $contexts['_hosts'][$alias][] = $context->get('key');
+                        }
+                    }
+                    
+                    // add context settings
+                    $contexts[$context->get('key')] = $contextSettings;
                 }
             }
                          
             unset($contextsGraph);
             $modx->cacheManager->set($cacheKey, $contexts, 0, $cacheOptions);
         }
-        
+
+
         if (!empty($contexts)) {
-        	$http_host = $_SERVER['HTTP_HOST'];
-        	if ($modx->getOption('xrouting.include_www', null, true)) {
-	        	$http_host = str_replace('www.','',$http_host);
-        	}
-            $requestUrl = $_REQUEST[$modx->getOption('request_param_alias', null, 'q')];
+            $http_host = $_SERVER['HTTP_HOST'];
+            if ($modx->getOption('xrouting.include_www', null, true)) {
+                $http_host = str_replace('www.','',$http_host);
+            }
+            
+            $modx_base_url = $modx->getOption('base_url', null, MODX_BASE_URL);
+            $requestUrl = str_replace('//','/',$modx_base_url.$_REQUEST[$modx->getOption('request_param_alias', null, 'q')]);
             $matches = array();
             
             
-            // find matching hosts
+        // find matching hosts
             $matched_contexts = $contexts['_hosts'][$http_host];
-            
             
             
             foreach ((array) $matched_contexts as $index => $ckey) {
                 
                 $context = $contexts[$ckey];
-                $strpos = strpos(MODX_BASE_URL.$requestUrl, $contexts[$ckey]['base_url']);
-                
+                $strpos = strpos($requestUrl, $contexts[$ckey]['base_url']);
+                //echo $requestUrl . ' - '. $contexts[$ckey]['base_url']."<br>";
                 if ($strpos === 0) {
                     $matches[strlen($contexts[$ckey]['base_url'])] = $ckey;
                 }
             }
-            
-//die('<pre>'.print_r($matches,true));
 
+        // modify request for the matched context
             if (!empty($matches)) {
-            	
-            	$cSettings = $contexts[$matches[max(array_keys($matches))]];
-            	$cKey = $matches[max(array_keys($matches))];
-            	
-            	
+                
+                $cSettings = $contexts[$matches[max(array_keys($matches))]];
+                $cKey = $matches[max(array_keys($matches))];
+                
                 // do we need to switch the context?
                 if ($modx->context->get('key') != $cKey) {
                     $modx->switchContext($cKey);
                 }
                 
                 // remove base_url from request query
-                if ($cSettings['base_url'] != MODX_BASE_URL) {
-                    $newRequestUrl = str_replace($cSettings['base_url'],'',MODX_BASE_URL.$requestUrl);
+                if ($cSettings['base_url'] != $modx_base_url) {
+                    $newRequestUrl = str_replace($cSettings['base_url'],'',$requestUrl);
                     $_REQUEST[$modx->getOption('request_param_alias', null, 'q')] = $newRequestUrl;
                 }
                 
                 
-            } else {
+            } else if ($_REQUEST['xrouting-debug'] != '1' || !$modx->getOption('xrouting.allow_debug_info', null, false)) {
                 // if no match found
                 if ($modx->getOption('xrouting.show_no_match_error', null, true)) {
-	                $modx->sendErrorPage();
+                    $modx->sendErrorPage();
                 } else {
-	                $modx->switchContext($modx->getOption('xrouting.default_context', null, 'web'));
+                    $modx->switchContext($modx->getOption('xrouting.default_context', null, 'web'));
                 }
                 
+            }
+        
+        // output debug info
+            if ($_REQUEST['xrouting-debug'] == '1' && $modx->getOption('xrouting.allow_debug_info', null, false)) {
+                $debuginfo = '<pre>';
+                $debuginfo .= "## MODX context map:\n\n" . print_r($contexts,true) . "\n\n\n";
+                $debuginfo .= "## Requested URL: " . $_REQUEST[$modx->getOption('request_param_alias', null, 'q')] . "\n\n\n";
+                $debuginfo .= "## Requested URL with base_url: ". $requestUrl ."\n\n\n";
+                $debuginfo .= "## Matched context(s) (Array key defines match quality):\n\n" . print_r($matches,true) . "\n\n\n";
+                $debuginfo .= "## Request will go to context: " . $matches[max(array_keys($matches))] . "\n\n\n";
+                $debuginfo .= "## Modified request URL: " . $newRequestUrl . "\n\n\n";
+                die($debuginfo);
             }
         }
     break;
 }
-return;
