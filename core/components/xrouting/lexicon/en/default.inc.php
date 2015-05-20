9fd61a843984b9763027a5e263b52c59784b02a8
Christian Seel <cs@chsmedien.de>
2015-05-20 11:00:35 +0200

    added lexicon for new setting

--- core/components/xrouting/lexicon/en/default.inc.php
+++ core/components/xrouting/lexicon/en/default.inc.php
@@ -10,3 +10,5 @@ $_lang['setting_xrouting.default_context'] = 'Default context';
 $_lang['setting_xrouting.default_context_desc'] = 'The default context to redirect to if no matches were found and xrouting.show_no_match_error is set to No.';
 $_lang['setting_xrouting.show_no_match_error'] = 'Return error messages';
 $_lang['setting_xrouting.show_no_match_error_desc'] = 'If set to yes, XRouting will return an error instead of redirecting to the default context.';
+$_lang['setting_xrouting.allow_debug_info'] = 'Allow debug output';
+$_lang['setting_xrouting.allow_debug_info_desc'] = 'Enable this setting to output debug info if the &xrouting-debug=1 GET parameter is set. ATTENTION: disable it again after you debugged your installation, since this exposes a lot information of project!';
